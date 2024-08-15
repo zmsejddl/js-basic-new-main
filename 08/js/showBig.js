d2e7bd02007649ac1eeb1dd12f40d86dddde1956
Jung Young Hun <hujung0268@gamil.com>
2024-08-15 12:04:01 +0900

    선택한 상품 이미지 표시하기

---
+++ 08/js/showBig.js
@@ -0,0 +1,13 @@
+// 1단계 태그 속성을 사용해 상품 이미지 변경하기
+var bigPic = document.querySelector("#cup"); // 큰 이미지 가져옴
+var smallPics = document.querySelectorAll(".small"); // 작은 이미지를 가져옴(노드 리스트)
+
+for (var i = 0; i < smallPics.length; i++) {
+  // 노드 리스트의 각 요소에 접근
+  smallPics[i].onclick = showBig; // 요소를 누르면 showBig() 함수 실행
+}
+
+function showBig() {
+  var newPic = this.src; // click 이벤트가 발생한 대상의 src 속성 값 가져옴
+  bigPic.setAttribute("src", newPic); // newPic 값을 큰 이미지의 src 속성에 할당
+}
