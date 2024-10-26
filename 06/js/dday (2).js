b0cde4f9f6e2c84e80e120df60b1c8c1a69d5c4d
Jung Young Hun <hujung0268@gamil.com>
2024-10-26 12:11:20 +0900

    기념일 계산 프로그램 만들기

---
+++ 06/js/dday (2).js
@@ -0,0 +1,22 @@
+var now = new Date();
+var firstDay = new Date("2018-03-23");
+var toNow = now.getTime();
+var toFirst = firstDay.getTime();
+var passedTime = toNow - toFirst;
+var passedDay = Math.round(passedTime / (1000 * 60 * 60 * 24));
+document.querySelector("#accent").innerHTML = passedDay + "일";
+
+calcDate(100);
+calcDate(200);
+calcDate(365);
+calcDate(500);
+
+function calcDate(days) {
+  var future = toFirst + days * (1000 * 60 * 60 * 24);
+  var someday = new Date(future);
+  var year = someday.getFullYear();
+  var month = someday.getMonth() + 1;
+  var date = someday.getDate();
+  document.querySelector("#date" + days).innerText =
+    year + "년 " + month + "월 " + date + "일";
+}
