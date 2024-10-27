b0dae75ff2bf0bd8a14ce95ef30d93c448b4d405
Jung Young Hun <hujung0268@gamil.com>
2024-10-27 12:54:37 +0900

    [상세 설명 보기/닫기] 버튼 만들기

--- 08/js/product (2).js
+++ 08/js/product (2).js
@@ -1,9 +1,23 @@
-		var bigPic = document.querySelector("#cup");  
-		var smallPics = document.querySelectorAll(".small");  
+var isOpen = false;
+var bigPic = document.querySelector("#cup");
+var smallPics = document.querySelectorAll(".small");
 
-		for(i=0; i<smallPics.length; i++) {
-			smallPics[i].addEventListener("click", function() {
-				newPic = this.src;
-				bigPic.setAttribute("src", newPic);
-			});
-		}
+for (i = 0; i < smallPics.length; i++) {
+  smallPics[i].addEventListener("click", function () {
+    newPic = this.src;
+    bigPic.setAttribute("src", newPic);
+  });
+}
+
+var view = document.querySelector("#view");
+view.addEventListener("click", function () {
+  if (isOpen == false) {
+    document.querySelector("#detail").style.display = "block";
+    view.innerText = "상세 설명 닫기";
+    isOpen = true;
+  } else {
+    document.querySelector("#detail").style.display = "none";
+    view.innerText = "상세 설명 보기";
+    isOpen = false;
+  }
+});
