d8fb21caaf7c2d38a3a437c44e6b997d6a4ee3e1
Jung Young Hun <hujung0268@gamil.com>
2024-10-27 13:07:56 +0900

    참가 신청 명단 프로그램 개선하기

--- 08/js/register (2).js
+++ 08/js/register (2).js
@@ -4,7 +4,23 @@ function newRegister() {
   var newText = document.createTextNode(userName.value);
   newP.appendChild(newText);
 
+  var delBttn = document.createElement("span");
+  var delText = document.createTextNode("X");
+  delBttn.setAttribute("class", "del");
+  delBttn.appendChild(delText);
+  newP.appendChild(delBttn);
+
   var nameList = document.querySelector("#nameList");
-  nameList.appendChild(newP);
+  nameList.insertBefore(newP, nameList.childNodes[0]);
   userName.value = "";
+
+  var removeBttns = document.querySelectorAll(".del");
+
+  for (var i = 0; i < removeBttns.length; i++) {
+    removeBttns[i].addEventListener("click", function () {
+      if (this.parentNode.parentNode) {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+      }
+    });
+  }
 }
