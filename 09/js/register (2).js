71c3a62f687e157fdf684b8f6003ed616bfd7585
Jung Young Hun <hujung0268@gamil.com>
2024-10-28 12:54:01 +0900

    입력한 검증 프로그램 만들기

---
+++ 09/js/register (2).js
@@ -0,0 +1,30 @@
+var userId = document.querySelector("#user-id");
+var pw1 = document.querySelector("#user-pw1");
+var pw2 = document.querySelector("#user-pw2");
+
+userId.onchange = checkId;
+pw1.onchange = checkPw;
+pw2.onchange = comparePw;
+
+function checkId() {
+  if (userId.value.length < 4 || userId.value.length > 15) {
+    alert("4~15자리의 영문과 숫자를 사용하세요.");
+    userId.select();
+  }
+}
+
+function checkPw() {
+  if (pw1.value.length < 8) {
+    alert("비밀번호는 8자리 이상이어야 합니다.");
+    pw1.value = "";
+    pw1.focus();
+  }
+}
+
+function comparePw() {
+  if (pw1.value != pw2.value) {
+    alert("암호가 다릅니다. 다시 입력하세요.");
+    pw2.value = "";
+    pw2.focus();
+  }
+}
