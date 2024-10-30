d66ba17b29ca864be143c38d4db81d9edaba8300
Jung Young Hun <hujung0268@gamil.com>
2024-10-30 10:35:52 +0900

    연습문제 2

---
+++ 07/js/quiz-2 (2).js
@@ -0,0 +1,10 @@
+var arr = [];
+arr[0] = "안녕하세요1";
+arr[1] = "안녕하세요2";
+arr[2] = "안녕하세요3";
+arr[3] = "안녕하세요4";
+arr[4] = "안녕하세요5";
+arr[5] = "안녕하세요6";
+
+var index = Math.floor(Math.random() * 6);
+document.write("<p>" + arr[index] + "</p>");
