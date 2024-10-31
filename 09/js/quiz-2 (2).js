5b9d7edef002b3afd76239fcfd9e8e04ac7385e2
Jung Young Hun <hujung0268@gamil.com>
2024-10-31 12:47:48 +0900

    연습문제 2

---
+++ 09/js/quiz-2 (2).js
@@ -0,0 +1,8 @@
+var radius = document.querySelector("#radius");
+var start = document.querySelector("#start");
+
+start.addEventListener("click", function () {
+  document.querySelector("#round").value = Number(radius.value) * Math.PI * 2;
+  document.querySelector("#area").value =
+    Number(radius.value) * Number(radius.value) * Math.PI;
+});
