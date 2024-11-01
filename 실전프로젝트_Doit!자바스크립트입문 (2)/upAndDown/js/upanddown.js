2142d686c03188450a491b2d9039f53b153aa98e
Jung Young Hun <hujung0268@gamil.com>
2024-11-01 11:49:26 +0900

    2. 사용자가 입력한 숫자 가져오기

--- 실전프로젝트_Doit!자바스크립트입문 (2)/upAndDown/js/upanddown.js
+++ 실전프로젝트_Doit!자바스크립트입문 (2)/upAndDown/js/upanddown.js
@@ -1,2 +1,12 @@
 var count = 0;
 var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+function finding() {
+  var userNumber = document.querySelector("#try").value;
+
+  if (userNumber >= 1 && userNumber <= 100) {
+    console.log(userNumber);
+  } else {
+    alert("1과 100 사이의 숫자를 입력하세요.");
+  }
+}
