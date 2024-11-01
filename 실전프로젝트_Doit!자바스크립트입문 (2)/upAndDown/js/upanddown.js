4fdaafb6ecc529d09ffaa4bea05dad6879afd1e5
Jung Young Hun <hujung0268@gamil.com>
2024-11-01 11:54:34 +0900

    3. 사용자 숫자와 컴퓨터 숫자 비교하기

--- 실전프로젝트_Doit!자바스크립트입문 (2)/upAndDown/js/upanddown.js
+++ 실전프로젝트_Doit!자바스크립트입문 (2)/upAndDown/js/upanddown.js
@@ -5,7 +5,17 @@ function finding() {
   var userNumber = document.querySelector("#try").value;
 
   if (userNumber >= 1 && userNumber <= 100) {
-    console.log(userNumber);
+    if (randomNumber > userNumber) {
+      document.querySelector("#display").innerHTML = "UP!";
+    } else if (randomNumber < userNumber) {
+      document.querySelector("#display").innerHTML = "DOWN!";
+    } else {
+      document.querySelector("#display").innerHTML =
+        "<span style='color:red'>맞혔습니다!</span>";
+    }
+    document.querySelector("#try").value = "";
+    count++;
+    document.querySelector("#counter").innerHTML = "시도 횟수: " + count + "회";
   } else {
     alert("1과 100 사이의 숫자를 입력하세요.");
   }
