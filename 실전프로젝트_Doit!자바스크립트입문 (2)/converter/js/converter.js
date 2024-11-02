f7d0cdba8a9495a2b929f9aeddb24667b8dec9be
Jung Young Hun <hujung0268@gamil.com>
2024-11-02 12:11:42 +0900

    계산 값 반올림하기

--- 실전프로젝트_Doit!자바스크립트입문 (2)/converter/js/converter.js
+++ 실전프로젝트_Doit!자바스크립트입문 (2)/converter/js/converter.js
@@ -22,8 +22,8 @@ function exUnit() {
 
 function converter() {
   if (cToF) {
-    target.value = source.value * 1.8 + 32;
+    target.value = (source.value * 1.8 + 32).toFixed(2);
   } else {
-    target.value = (source.value - 32) / 1.8;
+    target.value = ((source.value - 32) / 1.8).toFixed(2);
   }
 }
