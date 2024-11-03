815a915a3a328744b9146930f801408448cabaf9
Jung Young Hun <hujung0268@gamil.com>
2024-11-03 12:32:23 +0900

    2. 가져온 시간 값을 1초씩 줄이기

--- 실전프로젝트_Doit!자바스크립트입문 (2)/timer/js/timer.js
+++ 실전프로젝트_Doit!자바스크립트입문 (2)/timer/js/timer.js
@@ -12,3 +12,21 @@ function startTimer() {
   }
   timer = setInterval(countTimer, 1000);
 }
+
+function countTimer() {
+  if (sec != 0) {
+    sec--;
+    document.querySelector("#display").innerText =
+      min + "분 " + sec + "초 남았습니다.";
+  } else {
+    if (min != 0) {
+      min--;
+      sec = 59;
+    } else {
+      clearInterval(timer);
+      document.querySelector("#display").innerText = "타이머 종료";
+      document.querySelector("#startMin").value = "";
+      document.querySelector("#startSec").value = "";
+    }
+  }
+}
